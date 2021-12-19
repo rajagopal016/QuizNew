db536fe955f8622bb43df2e206163e75dbaf2b3d
rajagopal016 <33349229+rajagopal016@users.noreply.github.com>
2021-12-19 19:42:52 +0530

    Update QuizzizApplication.java

--- src/main/java/com/simplilearn/Quizziz/QuizzizApplication.java
+++ src/main/java/com/simplilearn/Quizziz/QuizzizApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizzizApplication {
+public class QuizzizApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzizApplication.class, args);
